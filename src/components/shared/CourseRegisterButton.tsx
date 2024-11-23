bafc9f599fd175cb8c4a5d0c8a678f84ea730628
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-11-23 12:05:11 +0700

    Sửa lại url đăng ký ngay của trang basic

--- src/components/shared/CourseRegisterButton.tsx
+++ src/components/shared/CourseRegisterButton.tsx
@@ -9,7 +9,7 @@ interface CourseRegisterButtonProps {
 export default function CourseRegisterButton({ courseType }: CourseRegisterButtonProps) {
   return (
     <Link
-      href={`/auth/register?course=${courseType}`}
+      href={`/checkout/basic`}
       className="block w-full bg-primary text-secondary text-center py-4 rounded-full font-bold hover:bg-primary-light transition-all duration-300"
     >
       Đăng ký ngay
