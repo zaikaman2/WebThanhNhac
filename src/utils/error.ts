4423756a95ed655c69fe2c304b5ab37448fbfefd
hoconlinecungdpt <hoconlinecungdpt@gmail.com>
2025-08-17 15:47:27 +0700

    Update error.ts

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,7 +1,8 @@
 export const logError = (error: Error) => {
   // Thêm logic ghi log ở đây
   console.error('Application error:', error)
-  
+
   // Gửi lỗi đến service monitoring (trong tương lai)
   // await sentryClient.captureException(error)
+  
 } 
