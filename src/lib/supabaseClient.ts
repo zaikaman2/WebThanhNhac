e0a4037f3541b98f604364585bb44d7ce40344a2
zaikaman <91252891+zaikaman@users.noreply.github.com>
2025-08-17 15:42:29 +0700

    Remove secret keys

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://fgmmykvsbzfgpdloadvb.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnbW15a3ZzYnpmZ3BkbG9hZHZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIyNjE5MDQsImV4cCI6MjA0NzgzNzkwNH0.CAfqy9vv5pI0ncep0mjTT4hjJFPGGQFjDfp-mmnesxQ'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
